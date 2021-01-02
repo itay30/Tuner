175a4f14020e18aef246a2e1e60d54306b6eb975
itay <itaymazor303@gmail.com>
2021-01-02 21:29:05 +0200

    GuitarTuner.java and guitar_tuner.xml changes

--- app/src/main/java/com/example/tuner/GuitarTuner.java
+++ app/src/main/java/com/example/tuner/GuitarTuner.java
@@ -12,17 +12,6 @@ import be.tarsos.dsp.pitch.PitchDetectionHandler;
 import be.tarsos.dsp.pitch.PitchDetectionResult;
 import be.tarsos.dsp.pitch.PitchProcessor;
 
-class Ans {
-    Ans(int _diff, String _diffColor, NearestNote _nn) {
-        this.diff = _diff;
-        this.diffColor = _diffColor;
-        this.nn = _nn;
-    }
-    public NearestNote nn;
-    public int diff;
-    public String diffColor;
-}
-
 class NearestNote {
     NearestNote( double _hertz, char _note) {
         this.hertz = _hertz;
@@ -39,36 +28,51 @@ class NearestNote {
     }
 }
 
-class Helper {
-    public static Ans tuner_engine(double inputHertz) {
-        NearestNote nearest_note = new NearestNote(-1, ' ');
-        if (inputHertz > 350) {
-            return new Ans(0, "BLACK", nearest_note);
-        }
-        if (inputHertz >= 0 && inputHertz < 96)
-            nearest_note = new NearestNote(82.4, 'e');
-        else if (inputHertz >= 96 && inputHertz < 128)
-            nearest_note = new NearestNote(110.0, 'A');
-        else if (inputHertz >= 128 && inputHertz < 171)
-            nearest_note = new NearestNote(146.8, 'D');
-        else if (inputHertz >= 171 && inputHertz < 221)
-            nearest_note = new NearestNote(196.0, 'G');
-        else if (inputHertz >= 221 && inputHertz < 287.5)
-            nearest_note = new NearestNote(246.9, 'B');
-        else
-            nearest_note = new NearestNote(329.6, 'E');
-
-        int diff = (int) (inputHertz - nearest_note.hertz);
-        if (Math.abs(diff) <= 1) {
-            return new Ans(diff, "GREEN", nearest_note);
-        } else if (Math.abs(diff) <= 3) {
-            return new Ans(diff, "YELLOW", nearest_note);
-        } else {
-            return new Ans(diff, "RED", nearest_note);
-        }
-
-    }
-}
+//class Helper {
+//    public static NearestNote getNearestNote(double inputHertz) {
+////        if (inputHertz > 350)
+////            return new Ans(0, "BLACK", nearest_note);
+////        if (inputHertz >= 0 && inputHertz < 96)
+////            nearest_note = new NearestNote(82.4, 'e');
+////        else if (inputHertz >= 96 && inputHertz < 128)
+////            nearest_note = new NearestNote(110.0, 'A');
+////        else if (inputHertz >= 128 && inputHertz < 171)
+////            nearest_note = new NearestNote(146.8, 'D');
+////        else if (inputHertz >= 171 && inputHertz < 221)
+////            nearest_note = new NearestNote(196.0, 'G');
+////        else if (inputHertz >= 221 && inputHertz < 287.5)
+////            nearest_note = new NearestNote(246.9, 'B');
+////        else
+////            nearest_note = new NearestNote(329.6, 'E');
+//        NearestNote nearest_note;
+//        if (inputHertz >= 70 && inputHertz < 96)
+//            nearest_note = new NearestNote(82.4, 'e');
+//        else if (inputHertz >= 96 && inputHertz < 125)
+//            nearest_note = new NearestNote(110.0, 'A');
+//        else if (inputHertz >= 130 && inputHertz < 160)
+//            nearest_note = new NearestNote(146.8, 'D');
+//        else if (inputHertz >= 180 && inputHertz < 210)
+//            nearest_note = new NearestNote(196.0, 'G');
+//        else if (inputHertz >= 230 && inputHertz < 260)
+//            nearest_note = new NearestNote(246.9, 'B');
+//        else if (inputHertz >= 315 && inputHertz < 345)
+//            nearest_note = new NearestNote(329.6, 'E');
+//        else
+//            nearest_note = new NearestNote(-1, ' ');
+//
+//        return nearest_note;
+//
+////        int diff = (int) (inputHertz - nearest_note.hertz);
+////        if (Math.abs(diff) <= 1) {
+////            return new Ans(diff, "GREEN", nearest_note);
+////        } else if (Math.abs(diff) <= 3) {
+////            return new Ans(diff, "YELLOW", nearest_note);
+////        } else {
+////            return new Ans(diff, "RED", nearest_note);
+////        }
+//
+//    }
+//}
 
 public class GuitarTuner extends MainActivity {
     @Override
@@ -78,7 +82,7 @@ public class GuitarTuner extends MainActivity {
 
         //Initializes the microphone for user input and text boxes for output on screen
         AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0);
-        final TextView current_hertz = findViewById(R.id.current_hertz);
+//        final TextView current_hertz = findViewById(R.id.current_hertz);
         final TextView expected_note = findViewById(R.id.expected_note);
         final TextView tuner_diff = findViewById(R.id.tuner_diff);
 
@@ -93,11 +97,22 @@ public class GuitarTuner extends MainActivity {
                         @Override
                         public void run() {
                             //Sets the text boxes to display according to the hertz that was played
-                            current_hertz.setText("" + String.format("%.1f", inputHertz));
-                            Ans answer = Helper.tuner_engine(inputHertz);
-                            expected_note.setText(answer.nn.toString());
-                            tuner_diff.setText("" + answer.diff);
-                            tuner_diff.setTextColor(Color.parseColor(answer.diffColor));
+//                            current_hertz.setText("" + String.format("%.1f", inputHertz));
+                            NearestNote nearestNote = getNearestNote(inputHertz);
+                            int diff = (int) (inputHertz - nearestNote.hertz);
+                            expected_note.setText("" + nearestNote.note);
+                            tuner_diff.setText("" + diff);
+                            if (Math.abs(diff) <= 1) {
+                                tuner_diff.setTextColor(Color.GREEN);
+                            } else if (Math.abs(diff) <= 3) {
+                                tuner_diff.setTextColor(Color.YELLOW);
+                            } else if (Math.abs(diff) <= 15) {
+                                tuner_diff.setTextColor(Color.RED);
+                            }
+                            else{
+                                expected_note.setText("");
+                                tuner_diff.setText("");
+                            }
                         }
                     });
                 }
@@ -109,5 +124,25 @@ public class GuitarTuner extends MainActivity {
         dispatcher.addAudioProcessor(p);
         //Starts the thread
         new Thread(dispatcher).start();
+    }
+
+    public static NearestNote getNearestNote(double inputHertz) {
+        NearestNote nearest_note;
+        if (inputHertz >= 70 && inputHertz < 96)
+            nearest_note = new NearestNote(82.4, 'e');
+        else if (inputHertz >= 96 && inputHertz < 125)
+            nearest_note = new NearestNote(110.0, 'A');
+        else if (inputHertz >= 130 && inputHertz < 160)
+            nearest_note = new NearestNote(146.8, 'D');
+        else if (inputHertz >= 180 && inputHertz < 210)
+            nearest_note = new NearestNote(196.0, 'G');
+        else if (inputHertz >= 230 && inputHertz < 260)
+            nearest_note = new NearestNote(246.9, 'B');
+        else if (inputHertz >= 315 && inputHertz < 345)
+            nearest_note = new NearestNote(329.6, 'E');
+        else
+            nearest_note = new NearestNote(-1, ' ');
+
+        return nearest_note;
     }
 }
